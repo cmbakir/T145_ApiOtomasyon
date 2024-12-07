ba84283cc4cd32ec060799c8bc6e6747710dabd0
cembakir <cembakir@hotmail.com>
2024-12-07 22:51:09 +0300

    v2

--- src/test/java/tests/C1_Get_ApiSorgulama.java
+++ src/test/java/tests/C1_Get_ApiSorgulama.java
@@ -1,42 +1,51 @@
 package tests;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class C1_Get_ApiSorgulama {
-    /*
-    https://restful-booker.herokuapp.com/booking/10 url'ine
-    bir GET request gonderdigimizde donen Response'un,
-    status code'unun 200,
-    ve content type'inin application/json; charset=utf-8,
-    ve Server isimli Header'in degerinin Cowboy,
-    ve status Line'in HTTP/1.1 200 OK
-    ve response suresinin 5 sn'den kisa oldugunu manuel olarak test ediniz.
+     /*
+     https://restful-booker.herokuapp.com/booking/10 url’ine
+     bir GET request gonderdigimizde donen Response’un,
+     status code’unun 200,
+     ve content type’inin application/json; charset=utf-8,
+     ve Server isimli Header’in degerinin Cowboy,
+     ve status Line’in HTTP/1.1 200 OK
+     ve response suresinin 5 sn’den kisa oldugunu manuel olarak test ediniz.
 
- */
+  */
+
     /*
-    Tum API Testleri
+        TÜM API Testleri otomasyon ile çalıştırılırken 4 adım izlenir
+            1- EndPoint hazırlanır
+            2- Soruda verilmiş ise Expected Data Hazırlanır. Verilmemişse bu adım atlanır
+            3- Request karşılığı olarak gelen response bir obje oluşlturularak içine kaydedilir
+            4-Assertion işlemi yapılır
      */
 
     @Test
     public void GetApiTest() {
-        String url ="https://restful-booker.herokuapp.com/booking/10";
+        //  1- EndPoint hazırlanır
+        String url = "https://restful-booker.herokuapp.com/booking/10";
 
+        // 2- Soruda verilmiş ise Expected Data Hazırlanır. Verilmemişse bu adım atla
+        //verilmemiş
 
+        // 3- Request karşılığı olarak gelen response bir obje oluşlturularak içine kaydedilir
         Response response = given().when().get(url);
 
-        //response.prettyPrint();
 
-        //response.prettyPeek();
+        // response.prettyPrint(); Sadece cevap datası yazdırır
+        // response.prettyPeek(); Cevabı Header değerleri ile birlikte yazdırır
 
-        System.out.println("Status Code:" + response.getStatusCode());
-        System.out.println("Content type:" + response.getContentType());
-        System.out.println("Header [Server]:" + response.getHeader("Server"));
-        System.out.println("Status Line:" + response.getStatusLine());
-        System.out.println("Response Time:" + response.getTime());
-
+        //4-Assertion işlemi yapılır
+        System.out.println("Status Code: " + response.getStatusCode()); //200
+        System.out.println("Content Type: " + response.getContentType()); //application/json; charset=utf-8
+        System.out.println("Header[Server]: " + response.getHeader("Server")); //Cowboy
+        System.out.println("Status Line: " + response.getStatusLine()); //HTTP/1.1 200 OK
+        System.out.println("Response Time: " + response.getTime()); //1877 ms < 5 s
 
 
     }
--- src/test/java/tests/C2_Get_ResponseBilgileriAssertion.java
+++ src/test/java/tests/C2_Get_ResponseBilgileriAssertion.java
@@ -1,49 +1,45 @@
 package tests;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
+
 public class C2_Get_ResponseBilgileriAssertion {
-     /*
-    https://restful-booker.herokuapp.com/booking/10 url'ine
-    bir GET request gonderdigimizde donen Response'un,
-    status code'unun 200,
-    ve content type'inin application/json; charset=utf-8,
-    ve Server isimli Header'in degerinin Cowboy,
-    ve status Line'in HTTP/1.1 200 OK
-    ve response suresinin 5 sn'den kisa oldugunu olarak test ediniz.
+    /*
+         https://restful-booker.herokuapp.com/booking/10 url’ine
+         bir GET request gonderdigimizde donen Response’un,
+         status code’unun 200,
+         ve content type’inin application/json; charset=utf-8,
+         ve Server isimli Header’in degerinin Cowboy,
+         ve status Line’in HTTP/1.1 200 OK
+         ve response suresinin 5 sn’den kisa oldugunu test ediniz.
 
- */
-
+    */
     @Test
     public void test(){
-        //1-EndPoint Hazırlanır
+        //1- EndPoint hazırlanır,
         String url="https://restful-booker.herokuapp.com/booking/10";
 
-        //2-ExpectedData hazırlanır
+        //2- ExpectedData hazırlanır
 
+        //3- Dönen Response bir obje içine kaydedilir
+        Response resp=given().when().get(url);
 
-        //3- Donen Response bir obje içine kaydedilir
-
-        Response resp = given().when().get(url);
-
-        //4-Asset
-
+        //4-Assertion işlemi yapılır
         resp.then().assertThat().statusCode(200)
                 .contentType("application/json; charset=utf-8")
                 .header("Server","Cowboy")
                 .statusLine("HTTP/1.1 200 OK");
-
-        long respTime= resp.getTime();
-        if(respTime<5000){
+        System.out.println();
+        long respTime=resp.getTime();
+        if (respTime<5000){
             System.out.println("Hata Bulunamadı\nResponse süresi 5 saniyeden kısadır");
-        } else {
-            System.out.println("Assertion işlemi REsponse Time acısından fail oldu");
+        }else{
+            System.err.println("HATA: Assertion işlemi Response Time açısından fail oldu");
         }
 
 
     }
 }
-
